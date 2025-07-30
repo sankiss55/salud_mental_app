5f5c06f164199506ad6a623e392574bd6fb8cd02
santy <166449953+sankiss55@users.noreply.github.com>
2025-07-30 21:38:18 +0000

    agregar readme

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# salud_mental_app
+# PSM (Priorizando mi Salud Mental)
+
+Proyecto universitario para brindar consejos y apoyo en diversos temas relacionados con la salud mental, tales como:
+
+- Ansiedad  
+- Calidad del sueño  
+- TDAH  
+- Estrés  
+- Regulación emocional  
+- Habilidades sociales  
+
+Además, la aplicación incluye función de lectura en voz alta con opciones de voces personalizables.
+
+---
+
+## Tecnologías
+
+<p align="center">
+  <img src="https://reactnative.dev/img/header_logo.svg" alt="React Native" width="120" />
+  &nbsp;&nbsp;&nbsp;&nbsp;
+  <img src="https://avatars.githubusercontent.com/u/12504344?s=280&v=4" alt="Expo" width="120" />
+</p>
+
+Esta aplicación está desarrollada con **React Native** utilizando **Expo Go**, una librería que facilita la visualización y pruebas en dispositivos Android e iOS.  
+La versión del SDK usada es la **53** de Expo.
+
+---
+
+## Cómo usar / Deployment
+
+Para ejecutar este proyecto localmente, sigue estos pasos:
+
+1. **Instala las dependencias**
+
+```bash
+npm install
+```
+2. **Ejecuta la aplicación**
+
+```bash
+npx expo start
+```
+3. **Opcional: Ejecutar con conexión pública**
+
+```bash
+npx expo start --tunnel
+```
+
+## Capturas de la aplicación
+<p align="center">
+  <img src="./imgenes_proyecto/IMG-20250730-WA0007.jpg" alt="React Native" width="120" />
+  &nbsp;&nbsp;&nbsp;&nbsp;
+  <img src="./imgenes_proyecto/IMG-20250730-WA0005.jpg" alt="Expo" width="120" />
+   &nbsp;&nbsp;&nbsp;&nbsp;
+  <img src="./imgenes_proyecto/IMG-20250730-WA0006.jpg" alt="Expo" width="120" />
+  
+   &nbsp;&nbsp;&nbsp;&nbsp;
+  <img src="./imgenes_proyecto/IMG-20250730-WA0008.jpg" alt="Expo" width="120" />
+  
+   &nbsp;&nbsp;&nbsp;&nbsp;
+  <img src="./imgenes_proyecto/IMG-20250730-WA0010.jpg" alt="Expo" width="120" />
+</p>
--- bienestar/App.js
+++ bienestar/App.js
@@ -1,24 +1,33 @@
+// 👇 Estas dos importaciones DEBEN ir primero
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
+import { enableScreens } from 'react-native-screens';
+enableScreens();
 
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import Inicio from './page/inicio';
 import Info_1 from './page/info_1';
 import Info_2 from './page/info_2';
 import Info_3 from './page/info_3';
 import Main from './page/main';
 import PagConcejos from './component/pag_concejos';
-const Stack=createStackNavigator();
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-<Stack.Navigator  initialRouteName='Inicio'>
-  <Stack.Screen name='Inicio' component={Inicio} options={{headerShown:false,}} />
-  <Stack.Screen name='Info_1' component={Info_1} options={{ headerShown:false}} />
-  <Stack.Screen name='Info_2' component={Info_2} options={{ headerShown:false}} />
-  <Stack.Screen name='Info_3' component={Info_3} options={{ headerShown:false}} />
-  <Stack.Screen name='main' component={Main} options={{ headerShown:false}} />
-  <Stack.Screen name='PagConcejos' component={PagConcejos} options={{ headerShown:false}} />
-  </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Inicio" component={Inicio} options={{ headerShown: false }} />
+        <Stack.Screen name="Info_1" component={Info_1} options={{ headerShown: false }} />
+        <Stack.Screen name="Info_2" component={Info_2} options={{ headerShown: false }} />
+        <Stack.Screen name="Info_3" component={Info_3} options={{ headerShown: false }} />
+        <Stack.Screen name="main" component={Main} options={{ headerShown: false }} />
+        <Stack.Screen name="PagConcejos" component={PagConcejos} options={{ headerShown: false }} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
